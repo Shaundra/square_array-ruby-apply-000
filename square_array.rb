ce8178d90dcf98db3a88c129d805eea205e1d4ec
Shaundra <s.m.crittenden@gmail.com>
2018-11-14 18:24:19 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-# def square_array(array)
-#   arr_squared = []
-#   array.each { |x| arr_squared << x ** 2 }
-#   arr_squared
-# end
-
 def square_array(array)
-  array.collect { |x| x ** 2 }
+  arr_squared = []
+  array.each { |x| arr_squared << x ** 2 }
+  arr_squared
 end
+
+# def square_array(array)
+#   array.collect { |x| x ** 2 }
+# end
